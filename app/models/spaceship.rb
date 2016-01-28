9b89ccbd612ae948d0a6e87d5c127017346999a1
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-01-28 19:13:20 +0000

    added methods for captain, tests passing and delta completed

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,10 @@
 class Spaceship < ActiveRecord::Base
 
-  has_many :crewing
+  has_many :crewings
   has_many :astronauts, :through => :crewings
+
+  def captain
+    crewings.where(captain: true).first.astronaut
+  end
 
 end
